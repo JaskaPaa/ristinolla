cb53e3dfc0b48a448262b74bfc909560c1b66549
Jaska Pääkkönen <jaska.paakkonen@gmail.com>
2024-04-29 16:56:30 +0300

    removes

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,175 +1,167 @@
 <script>
-  
-    import SkewedButton from '$lib/SkewedButton.svelte';
-    import Square from '$lib/Square.svelte';
-    import Sun from '$lib/Sun.svelte';
+	import SkewedButton from '$lib/SkewedButton.svelte';
+	import Square from '$lib/Square.svelte';
+	import Sun from '$lib/Sun.svelte';
 
-    import { onMount } from 'svelte';
+	import { onMount } from 'svelte';
 
-    import { gameBackground, gameLineColor, theme, vh, vw } from '$lib/stores.js';
+	import { gameBackground, gameLineColor, theme, vh, vw } from '$lib/stores.js';
 
-    let squares = ['X', 'O', 'X', 'O', 'X', 'O', 'X', 'O', 'X', 'O'];
-    let bgColor = "blue";
+	let squares = ['X', 'O', 'X', 'O', 'X', 'O', 'X', 'O', 'X', 'O'];
+	let bgColor = 'blue';
 
-    console.log("in layout: " + $theme);
+	console.log('in layout: ' + $theme);
 
-    $: if ($theme === `dark`) { 
-      gameBackground.set("#22222288")
-      gameLineColor.set("#55555588");
-    } else {
+	$: if ($theme === `dark`) {
+      gameBackground.set('#22222288');
+      gameLineColor.set('#55555588');
+	} else {
       //gameBackground.set("#88888855");
-      gameLineColor.set("#55555588");
-    }
+      gameLineColor.set('#55555588');
+	}
 
-    
+	function switchTheme() {
+      $theme = $theme === 'dark' ? 'light' : 'dark';
+      document.documentElement.classList.remove('dark-theme', 'light-theme');
+      document.documentElement.classList.add($theme + '-theme');
+      showMenu = false;
+	}
 
-    function switchTheme() {
-      $theme = ($theme === "dark") ? "light" : "dark";
-      document.documentElement.classList.remove("dark-theme", "light-theme");
-      document.documentElement.classList.add( $theme + "-theme"); 
-      showMenu = false;  
-	  }
+	let showMenu = false;
+	let skew = 20;
+	let burger = '&#9776;';
 
-    let showMenu = false;
-    let skew = 20; 
-    let burger = '&#9776;';
-
-    $: if  ($vw < $vh) { // portrait orientation
-            skew = 0;  
-          } else {
-            skew = 20;
-          }
-
-    function show() {
-      showMenu = (showMenu === true) ? false : true;
+	$: if ($vw < $vh) {
+      // portrait orientation
       skew = 0;
-	  }
+	} else {
+      skew = 20;
+	}
 
-    onMount(() => {
-      theme.subscribe(v => {
-        const bodyClassList = document.body.classList;
-        bodyClassList.remove('light-theme', 'system', 'dark-theme');
-        bodyClassList.add(v + "-theme");
+	function show() {
+      showMenu = showMenu === true ? false : true;
+      skew = 0;
+  }
+
+	onMount(() => {
+      theme.subscribe((v) => {
+          const bodyClassList = document.body.classList;
+          bodyClassList.remove('light-theme', 'system', 'dark-theme');
+          bodyClassList.add(v + '-theme');
       });
-	  });
-    
+	});
 </script>
 
 <svelte:head>
-	{#if $theme !== 'custom'}
-	<link rel="stylesheet" href="{$theme}-theme.css">
-	{:else}
-	<style>:root { background: purple; }</style>
-	{/if}
+    {#if $theme !== 'custom'}
+        <link rel="stylesheet" href="{$theme}-theme.css" />
+    {:else}
+        <style>
+            :root {
+              background: purple;
+            }
+        </style>
+    {/if}
 </svelte:head>
 
 <div class="foo">
-<nav>
-  <SkewedButton on:click={() => showMenu = false} skew={skew} float="left"><a class="theme-colors" href="/">Pelaa</a> </SkewedButton>
-  <span class="menu2">
-  {#each squares as square, j}
-    <Square onClick={() => {}}
-      text={square}
-      size={40}
-      />      		
-	{/each} 
-  </span>
-  <span class="burger">
-    <SkewedButton on:click={show} skew={skew}
-      float="right">{@html (showMenu) ? '&#x2715;' : '&#9776;' }</SkewedButton>
-  </span>
-  <span class:show={showMenu} class="menu">
-  <SkewedButton on:click={switchTheme} skew={-skew} float="right"> <Sun /> </SkewedButton>  
-  <SkewedButton on:click={() => showMenu = false} skew={-skew} float="right"><a class="theme-colors" href="/settings">Asetukset</a> </SkewedButton>
-  <SkewedButton on:click={() => showMenu = false} skew={-skew} float="right"><a class="theme-colors" href="/about">Tietoja</a> </SkewedButton>
-  <SkewedButton on:click={() => showMenu = false} skew={-skew} float="right"><a class="theme-colors" href="/rules">Säännöt</a> </SkewedButton>
-  </span>  
-</nav>
+    <nav>
+        <SkewedButton on:click={() => (showMenu = false)} {skew} float="left">
+            <a class="theme-colors" href="/">Pelaa</a>
+        </SkewedButton>
+        <span class="menu2">
+          {#each squares as square, j}
+            <Square onClick={() => {}} text={square} size={40} />
+          {/each}
+        </span>
+        <span class="burger">
+          <SkewedButton on:click={show} {skew} float="right">{@html showMenu ? '&#x2715;' : '&#9776;'}</SkewedButton>
+        </span>
+        <span class:show={showMenu} class="menu">
+          <SkewedButton on:click={switchTheme} skew={-skew} float="right"><Sun /></SkewedButton>
+          <SkewedButton on:click={() => (showMenu = false)} skew={-skew} float="right">
+            <a class="theme-colors" href="/settings">Asetukset</a>
+          </SkewedButton>
+          <SkewedButton on:click={() => (showMenu = false)} skew={-skew} float="right">
+            <a class="theme-colors" href="/about">Tietoja</a>
+          </SkewedButton>
+          <SkewedButton on:click={() => (showMenu = false)} skew={-skew} float="right">
+            <a class="theme-colors" href="/rules">Säännöt</a>
+          </SkewedButton>
+        </span>
+    </nav>
 </div>
 
-<slot></slot>
-
+<slot />
 
 <style>
+	:global(body) {
+		margin: 0;
+	}
 
-    :global(body) {
-      margin: 0;
-      /*background-color: var(--bg);*/
-    }
-    .foo {
-      width: 70%;
-      margin: auto;
-    }
-    nav {
-      background-color: transparent;
-      display: flow-root;
-      /*color: white;*/
-      width: 100%;
-      margin: auto;
-      font-size: 200%;
-      box-sizing: border-box;
-      /*overflow: hidden;*/
-    }
-    a {
-        text-decoration: none;
-        background-color: transparent;
-    }
-    .burger {
-      display: none;
-      width: 6rem;
-      position: absolute;
-      float: right;
-      right: 0;
-    }
+	.foo {
+		width: 70%;
+		margin: auto;
+	}
+	nav {
+		background-color: transparent;
+		display: flow-root;
+		width: 100%;
+		margin: auto;
+		font-size: 200%;
+		box-sizing: border-box;
+	}
+	a {
+		text-decoration: none;
+		background-color: transparent;
+	}
+	.burger {
+		display: none;
+		width: 6rem;
+		position: absolute;
+		float: right;
+		right: 0;
+	}
 
-    .show {
-      display: grid;
-      position: absolute;
-      z-index: 1000;
-      float: right;
-      /*left: 100px;*/
-      right: 0;
-      top: 2.5rem;
-      /*background-color: rgba(200, 200, 200, 1.0);*/
-    }
+	.show {
+		display: grid;
+		position: absolute;
+		z-index: 1000;
+		float: right;
+		right: 0;
+		top: 2.5rem;
+	}
 
-    /*.right {
-      display: flex;
-      flex-direction: column;
-      height: 100%;
-    }*/
+	@media screen and (max-width: 100rem) {
+		.menu2 {
+			display: none;
+		}
+		.foo {
+			width: 100%;
+		}
+	}
 
-    @media screen and (max-width: 100rem) {        
-       	.menu2 {
-          display: none;
-        }
-        .foo {
-          width: 100%;
-        }
-	  }
+	@media screen and (min-width: 50rem) {
+		.menu {
+			display: initial;
+			position: relative;
+			right: 0;
+			top: 0;
+		}
+	}
 
-    @media screen and (min-width: 50rem) {        
-       	.menu {
-          display: initial;
-          position: relative;
-          right: 0;
-          top: 0;
-        }
-    }
-
-    @media screen and (max-width: 50rem) {
-        .foo {
-          width: 100%;
-        }      
-       	.menu {
-          display: none;
-        }
-        .show {
-          display: grid;
-        }
-        .burger {
-          display: grid;
-        } 
-    }
-
+	@media screen and (max-width: 50rem) {
+		.foo {
+			width: 100%;
+		}
+		.menu {
+			display: none;
+		}
+		.show {
+			display: grid;
+		}
+		.burger {
+			display: grid;
+		}
+	}
 </style>
