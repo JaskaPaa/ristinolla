706cb9118183651704af34d2f205725e68f1eb28
Jaska Pääkkönen <jaska.paakkonen@gmail.com>
2024-05-16 15:27:49 +0300

    new SVG icon

---
+++ src/lib/Backarrow.svelte
@@ -0,0 +1,17 @@
+<svg xmlns="http://www.w3.org/2000/svg"
+	
+	height="100%"
+	viewBox="0 0 100 100">
+    
+	<path
+		d=""
+	/>
+	<path d="M 10 40 L 40 10 L 40 30 C 90 20, 90 70, 90 90 
+		C 90 70 60 40 40 50 L 40 70 L 10 40" stroke-width="5%" stroke="currentColor"/>
+</svg>
+
+<style>
+    svg { 
+        fill: var(--color-white);
+    }
+</style>
---
+++ src/lib/Flag.svelte
@@ -0,0 +1,17 @@
+<svg xmlns="http://www.w3.org/2000/svg"
+	
+	height="100%"
+	viewBox="0 0 100 100">
+	<!--M 20 60 C 30 50, 60 50, 60 60 S 70 70, 90 60 L 20 20-->
+    <path d=    "M 20 20 C 30 10, 40 10, 60 20 S 70 20, 90 20 L 90 60
+                         C 80 70, 60 60, 60 60 S 30 40, 20 60 L 20 20"                 
+                  stroke="currentColor" stroke-width="5%"/>    
+    <path d="M 12 10 L 12 90" stroke="currentColor" stroke-width="5%" />    
+
+</svg>
+
+<style>
+    svg { 
+        fill: var(--color-white);
+    }
+</style>
