44d60a9f58944f1d282919364c91ec263dfe25d3
Jaska Pääkkönen <jaska.paakkonen@gmail.com>
2024-05-16 15:57:22 +0300

    currentColor -> currentcolor

--- src/lib/Backarrow.svelte
+++ src/lib/Backarrow.svelte
@@ -7,11 +7,11 @@
 		d=""
 	/>
 	<path d="M 10 40 L 40 10 L 40 30 C 90 20, 90 70, 90 90 
-		C 90 70 60 40 40 50 L 40 70 L 10 40" stroke-width="5%" stroke="currentColor"/>
+		C 90 70 60 40 40 50 L 40 70 L 10 40" stroke-width="5%" stroke="currentcolor" />
 </svg>
 
 <style>
     svg { 
-        fill: var(--color-white);
+        fill: currentcolor; /*var(--color-white);*/
     }
 </style>
--- src/lib/Flag.svelte
+++ src/lib/Flag.svelte
@@ -5,8 +5,8 @@
 	<!--M 20 60 C 30 50, 60 50, 60 60 S 70 70, 90 60 L 20 20-->
     <path d=    "M 20 20 C 30 10, 40 10, 60 20 S 70 20, 90 20 L 90 60
                          C 80 70, 60 60, 60 60 S 30 40, 20 60 L 20 20"                 
-                  stroke="currentColor" stroke-width="5%"/>    
-    <path d="M 12 10 L 12 90" stroke="currentColor" stroke-width="5%" />    
+                  stroke="currentcolor" stroke-width="5%"/>    
+    <path d="M 12 10 L 12 90" stroke="currentcolor" stroke-width="5%" />    
 
 </svg>
 
